8d42a798a0b54e95d1511d7bb6d3172e540d3322
Hans Isbrücker <hisbrucker@gmail.com>
2023-09-15 12:00:37 +0200

    Add model stubs

---
+++ CalendarManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarBuilder
+{
+    internal class CalendarManager
+    {
+        public CalendarManager()
+        {
+            
+        }
+
+        public Calendar CreateCalendar(int year, int month)
+        {
+            // get the weeknumbers using ISOWeek
+            // create a month (perhaps a month has weeks and weeks have days?)
+        }
+    }
+}
