76766772f61ebb72599278928527af0e6c737631
Hans Isbrücker <hisbrucker@gmail.com>
2023-09-18 10:58:27 +0200

    Create Day class

--- Model/Day.cs
+++ Model/Day.cs
@@ -8,5 +8,21 @@ namespace CalendarBuilder.Model
 {
     internal class Day
     {
+        public DayOfWeek DayOfWeek { get; init; }
+        public int DayOfMonth { get; init; }
+
+        public Day(DayOfWeek dayOfWeek, int dayOfMonth)
+        {
+            DayOfWeek = dayOfWeek;
+
+            if (dayOfMonth > 0 && dayOfMonth < 31) 
+            {
+                this.DayOfMonth = dayOfMonth;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(dayOfMonth));
+            }
+        }
     }
 }
