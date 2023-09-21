aac78dcddce08fc4a3b037e043396179946e8531
Hans Isbrücker <hisbrucker@gmail.com>
2023-09-21 13:41:29 +0200

    Add WeekNumber to Week

--- Model/Week.cs
+++ Model/Week.cs
@@ -3,17 +3,25 @@
     public class Week
     {
         public required Day[] Days { get; init; }
+        public int WeekNumber { get; init; }
 
         /// <summary>
         /// Initialize a <see cref="Week"/>. As often a month doesn't begin on Monday a week on the calendar doesn't need to have 7 days
         /// </summary>
         /// <param name="days"></param>
-        public Week(params Day[] days)
+        public Week(int weeknumber ,params Day[] days)
         {
+            if (weeknumber < 1 || weeknumber > 52)
+            {
+                throw new ArgumentOutOfRangeException(nameof(weeknumber));
+            }
+
             if (!days.Any() || days.Length > 7)
             {
                 throw new ArgumentException("Must provide between 1 and 7 days", nameof(days));
             }
+
+            WeekNumber = weeknumber;
             Days = days;
         }
     }
