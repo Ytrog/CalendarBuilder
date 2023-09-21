f80959c05e8bb23d44ad26499439fa6813420626
Hans Isbrücker <hisbrucker@gmail.com>
2023-09-21 15:07:27 +0200

    Fill the months in the toolstrip

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,18 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            toolStripContainer1 = new ToolStripContainer();
+            toolStrip1 = new ToolStrip();
+            toolStripLabel1 = new ToolStripLabel();
+            toolStripLabel2 = new ToolStripLabel();
+            tsMonth = new ToolStripComboBox();
+            toolStripButton1 = new ToolStripButton();
+            tsYears = new ToolStripTextBox();
+            toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            toolStripContainer1.SuspendLayout();
+            toolStrip1.SuspendLayout();
             SuspendLayout();
             // 
-            // Form1
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            toolStripContainer1.ContentPanel.Size = new Size(1047, 660);
+            toolStripContainer1.Dock = DockStyle.Fill;
+            toolStripContainer1.Location = new Point(0, 0);
+            toolStripContainer1.Name = "toolStripContainer1";
+            toolStripContainer1.Size = new Size(1047, 685);
+            toolStripContainer1.TabIndex = 0;
+            toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            toolStripContainer1.TopToolStripPanel.Controls.Add(toolStrip1);
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.None;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripLabel1, tsYears, toolStripLabel2, tsMonth, toolStripButton1 });
+            toolStrip1.Location = new Point(3, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(448, 25);
+            toolStrip1.TabIndex = 0;
+            // 
+            // toolStripLabel1
+            // 
+            toolStripLabel1.Name = "toolStripLabel1";
+            toolStripLabel1.Size = new Size(27, 22);
+            toolStripLabel1.Text = "Jaar";
+            // 
+            // toolStripLabel2
+            // 
+            toolStripLabel2.Name = "toolStripLabel2";
+            toolStripLabel2.Size = new Size(44, 22);
+            toolStripLabel2.Text = "Maand";
+            // 
+            // tsMonth
+            // 
+            tsMonth.Name = "tsMonth";
+            tsMonth.Size = new Size(121, 25);
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(88, 22);
+            toolStripButton1.Text = "Maak kalender";
+            // 
+            // tsYears
+            // 
+            tsYears.Name = "tsYears";
+            tsYears.Size = new Size(121, 25);
+            // 
+            // Main
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form1";
+            ClientSize = new Size(1047, 685);
+            Controls.Add(toolStripContainer1);
+            Name = "Main";
             Text = "Calendar Builder";
+            toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            toolStripContainer1.TopToolStripPanel.PerformLayout();
+            toolStripContainer1.ResumeLayout(false);
+            toolStripContainer1.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private ToolStripContainer toolStripContainer1;
+        private ToolStrip toolStrip1;
+        private ToolStripComboBox tsMonth;
+        private ToolStripLabel toolStripLabel1;
+        private ToolStripLabel toolStripLabel2;
+        private ToolStripButton toolStripButton1;
+        private ToolStripTextBox tsYears;
     }
 }
--- Main.cs
+++ Main.cs
@@ -1,10 +1,23 @@
+using System.Globalization;
+
 namespace CalendarBuilder
 {
     public partial class Main : Form
     {
         public Main()
         {
+            CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("nl-nl");
+            CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture;
             InitializeComponent();
+            tsYears.Text = DateTime.Now.Year.ToString();
+            FillMonths();
+        }
+
+
+        private void FillMonths()
+        {
+            tsMonth.Items.Clear();
+            tsMonth.Items.AddRange(DateTimeFormatInfo.CurrentInfo.MonthNames);
         }
     }
 }
