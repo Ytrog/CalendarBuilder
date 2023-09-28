78ff86cfbc9e1be62fc76804abc5401e0662ae89
Hans Isbrücker <hisbrucker@gmail.com>
2023-09-28 12:39:23 +0200

    Created WeekControl

---
+++ Controls/WeekControl.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace CalendarBuilder.Controls
+{
+    partial class WeekControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            lblWeekNumber = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 8;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.Controls.Add(lblWeekNumber, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(938, 210);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblWeekNumber
+            // 
+            lblWeekNumber.AutoSize = true;
+            lblWeekNumber.Location = new Point(3, 0);
+            lblWeekNumber.Name = "lblWeekNumber";
+            lblWeekNumber.Size = new Size(14, 90);
+            lblWeekNumber.TabIndex = 0;
+            lblWeekNumber.Text = "label1";
+            // 
+            // WeekControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel1);
+            Name = "WeekControl";
+            Size = new Size(938, 210);
+            Load += WeekControl_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label lblWeekNumber;
+    }
+}
---
+++ Controls/WeekControl.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalendarBuilder.Model;
+
+namespace CalendarBuilder.Controls
+{
+    public partial class WeekControl : UserControl
+    {
+        public Model.Week? Week { get; init; }
+
+        public WeekControl(Week? week)
+        {
+            Week = week;
+        }
+
+        public WeekControl()
+        {
+            InitializeComponent();
+        }
+
+        private void WeekControl_Load(object sender, EventArgs e)
+        {
+            if (Week != null)
+            {
+                lblWeekNumber.Text = Week.WeekNumber.ToString();
+
+                DayControl[] days = new DayControl[7];
+                
+
+                foreach (Model.Day day in Week.Days)
+                {
+                    switch (day.DayOfWeek)
+                    {
+                        case DayOfWeek.Sunday:
+                            days[7] = new DayControl(day);
+                            break;
+                        case DayOfWeek.Monday:
+                            days[0] = new DayControl(day);
+                            break;
+                        case DayOfWeek.Tuesday:
+                            days[1] = new DayControl(day);
+                            break;
+                        case DayOfWeek.Wednesday:
+                            days[2] = new DayControl(day);
+                            break;
+                        case DayOfWeek.Thursday:
+                            days[3] = new DayControl(day);
+                            break;
+                        case DayOfWeek.Friday:
+                            days[4] = new DayControl(day);
+                            break;
+                        case DayOfWeek.Saturday:
+                            days[5] = new DayControl(day);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                for (int i = 0; i < days.Length; i++)
+                {
+                    if (days[i] == null)
+                    {
+                        days[i] = new DayControl();
+                    }
+                }
+            }
+        }
+    }
+}
