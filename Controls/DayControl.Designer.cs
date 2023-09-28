f686bd41adac467f68bebccbf51c019b5528e441
Hans Isbrücker <hisbrucker@gmail.com>
2023-09-28 12:12:45 +0200

    Added DayControl

---
+++ Controls/DayControl.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CalendarBuilder.Controls
+{
+    partial class DayControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblDay = new Label();
+            SuspendLayout();
+            // 
+            // lblDay
+            // 
+            lblDay.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lblDay.AutoSize = true;
+            lblDay.Location = new Point(68, 10);
+            lblDay.Name = "lblDay";
+            lblDay.Size = new Size(38, 15);
+            lblDay.TabIndex = 0;
+            lblDay.Text = "label1";
+            // 
+            // DayControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(lblDay);
+            Name = "DayControl";
+            Size = new Size(175, 182);
+            Load += DayControl_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblDay;
+    }
+}
---
+++ Controls/DayControl.cs
@@ -0,0 +1,31 @@
+﻿using CalendarBuilder.Model;
+
+namespace CalendarBuilder.Controls
+{
+    public partial class DayControl : UserControl
+    {
+        public Model.Day? Day { get; init; }
+
+        public DayControl()
+        {
+            InitializeComponent();
+        }
+
+        public DayControl(Model.Day day) : this()
+        {
+            Day = day;
+        }
+
+        private void DayControl_Load(object sender, EventArgs e)
+        {
+            if (Day != null)
+            {
+                lblDay.Text = $"{Day.DayOfWeek} {Day.DayOfMonth}";
+            }
+            else
+            {
+                lblDay.Text = nameof(lblDay);
+            }
+        }
+    }
+}
