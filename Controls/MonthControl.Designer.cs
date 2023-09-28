d42f85f6f74e64a16347107d328e45b217beb2a2
Hans Isbrücker <hisbrucker@gmail.com>
2023-09-28 12:47:28 +0200

    Added MonthControl

---
+++ Controls/MonthControl.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace CalendarBuilder.Controls
+{
+    partial class MonthControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(643, 436);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // MonthControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(flowLayoutPanel1);
+            Name = "MonthControl";
+            Size = new Size(643, 436);
+            Load += MonthControl_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ Controls/MonthControl.cs
@@ -0,0 +1,39 @@
+﻿using CalendarBuilder.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalendarBuilder.Controls
+{
+    public partial class MonthControl : UserControl
+    {
+        public Model.Month? Month { get; init; }
+
+        public MonthControl(Month? month)
+        {
+            Month = month;
+        }
+
+        public MonthControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MonthControl_Load(object sender, EventArgs e)
+        {
+            if (Month != null)
+            {
+                foreach (Week week in Month.Weeks.OrderBy(w => w.WeekNumber)) 
+                {
+                    flowLayoutPanel1.Controls.Add(new WeekControl(week));
+                }
+            }
+        }
+    }
+}
